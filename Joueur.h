6ce4892b9975cf9306e6db5c0b515b94f3e64d26
RomainBoucard <101243946+RomainBoucard@users.noreply.github.com>
2022-05-20 08:00:47 +0000

    Create file class Joueur

---
+++ Joueur.h
@@ -0,0 +1,24 @@
+#ifndef cpp_Joueur_h
+#define cpp_Joueur_h
+
+#include <iostream>
+using namespace std;
+
+class Joueur {
+
+private :
+
+
+public :
+
+
+//Constructeurs
+
+//Getteur et Setteurs
+
+
+//Méthodes
+
+};
+
+#endif
