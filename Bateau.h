d524a639ab6d86bbb568a35a88e8dbe69d52ad01
RomainBoucard <101243946+RomainBoucard@users.noreply.github.com>
2022-05-20 08:49:33 +0000

    Create files class Bateau

---
+++ Bateau.h
@@ -0,0 +1,24 @@
+#ifndef cpp_Bateau_h
+#define cpp_Bateau_h
+
+#include <iostream>
+using namespace std;
+
+class Bateau {
+
+private :
+
+
+public :
+
+
+//Constructeurs
+
+//Getteur et Setteurs
+
+
+//Méthodes
+
+};
+
+#endif
